7c1b9b5f72d554bf234bcc865d6330adafbfce26
tshastri96 <66653934+tshastri96@users.noreply.github.com>
2020-06-23 17:55:25 -0500

    Corrected comments + code for new table

--- generate.R
+++ generate.R
@@ -8,22 +8,24 @@ suppressPackageStartupMessages(library("DT"))
 
 #####INPUT & PROCESS#####
 # obgyn:
-#   - add column for # of physicians over 60 per 100K population
-#   - only select states that can be plotted (remove DC, PR)
-#   - Adjusted number if the number of physicians per 100,000 people in the state
+#   - Read in "aamc-state-data.csv" file
+#   - Add column 'Adjusted number': number of physicians per 100,000 people in the state
+#   - add column 'risk': number of physicians over 60 per 100K population
+#   - only select states that can be plotted (remove DC, PR, AK, HI)
 obgyn <- read_csv("aamc-state-data.csv") %>%
   mutate(Adjust_Num = 1 / pts_per_obgyn * 100000,
          Risk = Adjust_Num * Over60 / 100) %>%
   filter(!(state %in% c("DC", "PR", "AK", "HI")))
 
 # covid:
+#   - Read in "covid-confirmed.csv" file
 #   - rename `Province_State` column to `region` (and make lowercase in process)
-#   - rename `6/4/2020` to `cases`
+#   - rename `6/21/20` to `cases`
 #   - only select date column that will actually be used
 #   - remove rows w/ zero cases (won't be plotted anyway, won't cause logarithm issues)
 #   - remove states not in continental U.S.
 #   - aggregate (sum) cases by state (`region`)
-#   - take logarithm to make bubbles more aesthetically pleasing
+#   - create new column for logarithm of cases
 covid <- read_csv("covid-confirmed.csv") %>%
   mutate(region = tolower(Province_State), Province_State = NULL,
          cases = `6/21/20`) %>%
@@ -48,7 +50,7 @@ state_sex <- read_csv("state_sex.csv") %>%
   filter(region %in% obgyn$region) %>%
   mutate(female = 1 - (mf_ratio / (mf_ratio + 1)))
 
-# merge datasets for bubble plot data (log covid cases represented by bubble radius)
+# merge datasets for bubble plot data
 bubble_data <- left_join(lat_long, covid)
 
 # prepare map data
@@ -58,15 +60,15 @@ state_data <- left_join(states_pleth, obgyn)
 #####VISUALIZE & SAVE#####
 ## bubble map of 50 U.S. states
 ggplot() +
-  # map w/ states colored by % `physicians over 60` per 100K population
+  # map w/ states colored by % `physicians over 60
   geom_polygon(data = state_data,
                aes(x = long, y = lat, group = group, fill = Over60),
                color = "black") +
-  scale_fill_gradient("% Ob/Gyn > 60 yo", low = "lightblue", high = "darkblue") +
-  # bubbles w/ log(COVID cases)
+  scale_fill_gradient("% Ob/Gyn > 60 yo", low = "antiquewhite1", high = "darkblue") +
+  # bubbles w/ # COVID cases as of June 21, 2020
   geom_point(data = bubble_data,
              aes(x = Longitude, y = Latitude, size = cases),
-             colour = "firebrick2", alpha = 0.45, shape=20) +
+             colour = "firebrick2", alpha = 0.8, shape=20) +
   scale_size_continuous("Confirmed cases",
                         range = c(1, 15),
                         breaks = 50000 * c(1, 2, 4, 6, 8),
@@ -114,12 +116,25 @@ mega_table <- left_join(obgyn, covid) %>%
          births_per_under60 = total_births / num_ob_under60,
          covid_per_under60 = cases / num_ob_under60 * female) %>%
   rename(State = region) %>%
-  select(-state, -pts_per_obgyn, -num_ob, -Over60, -Adjust_Num, -Risk,
-         -cases, -logcases, -total_births, -mf_ratio, -female) %>%
+  select(-state, -pts_per_obgyn, -Adjust_Num, -Risk,
+         -logcases, -total_births, -mf_ratio, -female) %>%
   write_csv(path = "mega-table.csv")
 
+#####HIGH RISK STATES TABLE#####
+# Create top 10 high risk states: sort in descending order by covid cases per OB/GYN over 60
+# Select columns for High Risk States Tables
+# Rename columns
+# Select top 10 states
+highriskstates <- mega_table[with(mega_table, order(-covid_per_over60, -Over60, -cases)), ]
+highriskstates <- highriskstates %>% 
+  select(State, Over60, num_ob, cases, covid_per_over60) %>%
+  rename("Percentage of OB/GYNs over 60" = Over60) %>%
+  rename("Number of Practicing OB/GYNs" = num_ob) %>%
+  rename("COVID-19 cases" = cases) %>%
+  rename("COVID-19 cases per OB/GYN over 60" = covid_per_over60)
+highriskstates <- highriskstates[1:10,]  
+  
 # code to create table image (cannot use for manuscript b/c need
-#   text-editable tables in MS Word and need to decide metric to sort by)
-#tbl1 <- ggplot() + annotation_custom(tableGrob(HighRiskStates, rows = NULL)) + ggtitle('High Risk States')
-#tbl2 <- ggplot() + annotation_custom(tableGrob(LowRiskStates, rows = NULL)) + ggtitle('Low Risk States')
-#ggsave(grid.arrange(tbl1, tbl2), filename = "RiskTable.png", width = 10, height = 5)
+# text-editable tables in MS Word and need to decide metric to sort by)
+tbl1 <- ggplot() + annotation_custom(tableGrob(highriskstates, rows = NULL)) + ggtitle('High Risk States')
+ggsave(grid.arrange(tbl1), filename = "RiskTable.png", width = 11.5, height = 4)
